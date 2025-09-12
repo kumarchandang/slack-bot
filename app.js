ae402f1082d25e199bbbcddd1566752d071997ec
Chandan Kumar <kumarchandang801@gmail.com>
2025-09-12 14:50:47 +0530

    setup the dotenv in local

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { App } = require('@slack/bolt');
 
 const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env['SLACK_SIGNING_SECRET'],
+  token: process.env['SLACK_BOT_TOKEN'],
 });
 
 /* Add functionality here */
